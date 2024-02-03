c351f574198bf3dc5efb5bd41d0e060c2824bf7b
MakEva <sher.evikm@gmail.com>
2024-02-04 01:02:52 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 import css from './styles.module.css';
@@ -6,18 +6,20 @@ import css from './styles.module.css';
 const modalRoot = document.getElementById('modal-root');
 
 export const Modal = ({ close, children }) => {
-  const closeModal = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
-      close();
-    }
-  };
+  const closeModal = useCallback(
+    ({ target, currentTarget, code }) => {
+      if (target === currentTarget || code === 'Escape') {
+        close();
+      }
+    },
+    [close]
+  );
 
   useEffect(() => {
     document.addEventListener('keydown', closeModal);
 
     return () => document.removeEventListener('keydown', closeModal);
-  }, [close]);
-
+  }, [closeModal]);
   return createPortal(
     <div className={css.overlay} onClick={closeModal}>
       <div className={css.modal}>{children}</div>
@@ -25,3 +27,16 @@ export const Modal = ({ close, children }) => {
     modalRoot
   );
 };
+
+// export const Modal = ({ close, children }) => {
+//   const closeModal = ({ target, currentTarget, code }) => {
+//     if (target === currentTarget || code === 'Escape') {
+//       close();
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener('keydown', closeModal);
+
+//     return () => document.removeEventListener('keydown', closeModal);
+//   }, [close]);
